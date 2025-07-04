3240160c47fe4b5383051c9082a0e17d3a6d3316
AUGUSTO PÉREZ RODRIGUEZ <48794823@ort>
2025-07-04 08:55:27 -0300

    ClaseBDaMedias

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,6 +1,15 @@
 using Microsoft.Data.SqlClient;
 using Dapper;
 
-public class BD{
+public static class BD{
+    private static string_connectionString = @"Server=localhost;DataBase=LaAUF;Integrated Security=True;TrustServerCertificate=True";
 
+    public Integrante LevantarIntegrante(string integrante){
+        Integrante integrante = null;
+        using(SqlConnction connection = new SqlConnction(_connectionString)){
+            string query = "...;
+            integrante= connection.QueryFirstOrDefault<Integrante>(query new {});
+        }
+        return integrante;
+    }
 }
