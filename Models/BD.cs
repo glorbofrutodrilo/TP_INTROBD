dd6f68ddeb2c1b6a69c57314ce462b7299a246d0
AUGUSTO PÉREZ RODRIGUEZ <48794823@ort>
2025-07-04 08:06:25 -0300

    ClasesCreadas

---
+++ Models/BD.cs
@@ -0,0 +1,6 @@
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+public class BD{
+
+}
---
+++ Models/Integrante.cs
@@ -0,0 +1,8 @@
+public class Integrante{
+    public string Nombre{get;set;}
+    public string Apellido {get;set;}
+    public int Edad {get;set;}
+    public string Email {get;set;}
+    public string Password {get;set;}
+
+}
