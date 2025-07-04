d4efc6b9dc3d3ceca2459c41c1137806ba55f9cb
AUGUSTO PÉREZ RODRIGUEZ <48794823@ort>
2025-07-04 08:39:30 -0300

    Fotos

--- obj/Debug/net8.0/TP_BD.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP_BD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_BD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd6f68ddeb2c1b6a69c57314ce462b7299a246d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_BD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_BD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
